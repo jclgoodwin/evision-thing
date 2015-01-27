db3ac89c26b7f7a045ad2fc78a354b1c092607c8
Josh Goodwin <j@joshuagoodw.in>
2015-01-27 12:22:58 +0000

    remove redundant {}s

--- evision-thing.sh
+++ evision-thing.sh
@@ -12,18 +12,17 @@ if [[ $USERNAME = '' || $PASSWORD = '' || $EMAIL = '' ]]; then
 fi
 
 OUTPUT=`casperjs evision-thing.js --username="$USERNAME" --password="$PASSWORD"`
-echo $OUTPUT;
 
 
 if [[ $OUTPUT == 'null' ]]; then
    echo 'There was a problem. Perhaps you are running this thing too frequently.'
    exit 1
 elif [ -a output-latest.txt ]; then # script has been run before
-    DIFF=`diff -U 4 output-latest.txt <(echo "${OUTPUT}")`
+    DIFF=`diff -U 4 output-latest.txt <(echo "$OUTPUT")`
     if [ ${PIPESTATUS[0]} -eq 1 ]; then
         echo 'Changes found, emailing'
-        echo "${DIFF}" | mail -s 'Exam update!' $EMAIL
+        echo "$DIFF" | mail -s 'Exam update!' $EMAIL
     fi
 fi
 
-echo "${OUTPUT}" > output-latest.txt
+echo "$OUTPUT" > output-latest.txt
